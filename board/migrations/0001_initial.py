606adc4fa765adc0f714a64b96076e413769c8fa
oldfoagie <akanorm@gmail.com>
2015-07-05 16:55:12 -0700

    My first commit

---
+++ board/migrations/0001_initial.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.utils.timezone
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Post',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=40)),
+                ('name', models.CharField(max_length=40)),
+                ('city', models.CharField(max_length=20)),
+                ('phone', models.CharField(max_length=30)),
+                ('email', models.EmailField(max_length=254)),
+                ('created_date', models.DateTimeField(default=django.utils.timezone.now)),
+                ('published_date', models.DateTimeField(null=True, blank=True)),
+                ('author', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+    ]
